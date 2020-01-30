f6f5ff0b392b66111d5c0260b315b12640fd4751
IshaniTrivedi99 <ishani.t@solutelabs.com>
2020-01-30 18:17:34 +0530

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/about', to: 'static_pages#about'
   get '/signup', to: 'users#new'
-
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
