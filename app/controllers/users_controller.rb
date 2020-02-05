e3438768be040d79cf53f0051fc747d4b094323b
IshaniTrivedi99 <ishani.t@solutelabs.com>
2020-02-05 12:02:51 +0530

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,12 @@ class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
   before_action :admin_user, only: :destroy
+
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
+
   def new
     @user = User.new
   end
@@ -60,6 +63,18 @@ class UsersController < ApplicationController
       end
 
       # Before filters
+
+      # Confirms the correct user.
+        def correct_user
+          @user = User.find(params[:id])
+          redirect_to(root_url) unless current_user?(@user)
+        end
+
+        # Confirms an admin user.
+        def admin_user
+          redirect_to(root_url) unless current_user.admin?
+        end
+
       # Confirms a logged-in user.
       def logged_in_user
         unless logged_in?
